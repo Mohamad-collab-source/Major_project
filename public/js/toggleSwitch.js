40b5576888651a72e135a59c2deb541d3c5e7bf6
mohamad-collab-source <mohamadzeeshanm@gmail.com>
2026-02-15 16:35:00 +0530

    fix js crash preventing map rendering

--- public/js/toggleSwitch.js
+++ public/js/toggleSwitch.js
@@ -1,14 +1,13 @@
- let Taxswitch = document.getElementById('switchCheckDefault');
-  Taxswitch.addEventListener('click',()=>{
-    let showgst = document.getElementsByClassName('show-gst');
-    for(gst of showgst){
-      if(gst.style.display!="inline"){
-        gst.style.display="inline";
-      }
-      else {
-        gst.style.display="none";
-      }
+document.addEventListener("DOMContentLoaded", () => {
+
+  let Taxswitch = document.getElementById("switchCheckDefault");
+
+  Taxswitch.addEventListener("click", () => {
+    let showgst = document.getElementsByClassName("show-gst");
+
+    for (let gst of showgst) {
+      gst.style.display = gst.style.display === "inline" ? "none" : "inline";
     }
-  })
+  });
 
-
+});
